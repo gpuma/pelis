59226ed4261cc428d4b5d8086b890e6a43bb1d67
Gustavo Puma <vash47@gmail.com>
2018-03-20 23:56:15 -0500

    fetching data successfully

---
+++ pelis/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using pelis.Models;
+
+namespace pelis.Controllers
+{
+    [Route("[controller]")]
+    public class MoviesController : Controller
+    {
+        private readonly PelisContext _context;
+
+        public MoviesController(PelisContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movies.ToListAsync());
+        }
+    }
+}
---
+++ pelis/Data/dummy-data.sql
@@ -0,0 +1,23 @@
+﻿insert into Movies values('Nicolas Winding Refn','Crime drama',2011,'Drive')
+insert into Movies values('Nicolas Winding Refn','Crime drama',2013,'Only God Forgives')
+insert into Movies values('Nicolas Winding Refn','Horror',2016,'The Neon Demon')
+insert into Movies values('Alex Garland','Sci-Fi',2014,'Ex Machina')
+insert into Movies values('Alex Garland','Sci-Fi',2018,'Annihilation')
+
+insert into Actors values('19801112','Ryan Gosling')
+insert into Actors values('19590811','Vithaya Pansringarm')
+insert into Actors values('19850528','Carey Mulligan')
+insert into Actors values('19980409','Elle Fanning')
+insert into Actors values('19790309','Oscar Isaac')
+insert into Actors values('19830512','Domhnall Gleeson')
+
+select * from Movies
+select * from Actors
+
+insert into MovieActor values(1,1)
+insert into MovieActor values(1,3)
+insert into MovieActor values(2,1)
+insert into MovieActor values(2,2)
+insert into MovieActor values(3,4)
+insert into MovieActor values(4,5)
+insert into MovieActor values(4,6)
